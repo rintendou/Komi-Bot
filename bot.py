9af50d604078c441a5802f9c1d86b2a02d371db7
Danny Chen <dannychen8102@gmail.com>
2022-04-09 19:07:33 -0400

    Added retweet method

--- bot.py
+++ bot.py
@@ -1,6 +1,9 @@
 from bot_functions.get_quote import quote_on_enable
 from bot_functions.reply_mention import komi_reply
+from bot_functions.retweet import retweet_on_enable
 
 while True:
     quote_on_enable()
     komi_reply()
+    retweet_on_enable()
+    
