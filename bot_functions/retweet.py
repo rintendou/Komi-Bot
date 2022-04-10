ba127ff5cf0cf35858a873d57581096ca0840533
Danny Chen <dannychen8102@gmail.com>
2022-04-09 23:32:28 -0400

    Added additional comments

--- bot_functions/retweet.py
+++ bot_functions/retweet.py
@@ -18,12 +18,12 @@ def retweet_on_enable():
             print(f"Mention tweet: {mention.author.screen_name} - {mention.text}")
             mention_id = mention.id
 
-            mention_analyzed = TextBlob(mention.text) # TextBlob API analyzing the polarity of a tweet
-            mention_polarity_score = mention_analyzed.polarity # Scoring hte analyzed tweet
+            mention_analyzed = TextBlob(mention.text) # TextBlob API analyzing the polarity of a tweet.
+            mention_polarity_score = mention_analyzed.polarity # Scoring hte analyzed tweet.
             print(f"Mention tweet has a sentiment value of: {mention_polarity_score}")
 
             if mention.in_reply_to_status_id is None and mention.author.id != bot_id: 
-                if mention_polarity_score >= 0.3 and not mention.retweeted: # Checking for positive  polarity score and assuring that it has not been retweeted already.
+                if mention_polarity_score >= 0.3 and not mention.retweeted: # Checking for positive polarity score and assuring that it has not been retweeted already.
                     try:
                         api.retweet(api.update_status(status = "．．．" + "\n\n" + "Komi-Translation: " + "\n" + ':)'))
                         print("Positive polarity detected, retweeting.")
