4733f8fbe0e8fa21e5a7adcda3eca8fbf1c17d7c
Danny Chen <dannychen8102@gmail.com>
2022-04-10 00:40:29 -0400

    Added get_gif() function to branch

--- bot.py
+++ bot.py
@@ -2,10 +2,11 @@ from bot_functions.get_quote import quote_on_enable
 from bot_functions.reply import reply_on_enable
 from bot_functions.retweet import retweet_on_enable
 from bot_functions.nuke import nuke
+from bot_functions.get_gif import get_gif
 
 while True:
     nuke()
+    get_gif()
     quote_on_enable()
     reply_on_enable()
     retweet_on_enable()
-    
