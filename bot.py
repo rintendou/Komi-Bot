9479136f9a15220657ef613bd36a25de39326516
Danny Chen <dannychen8102@gmail.com>
2022-04-10 00:01:39 -0400

    Added nuke method for debugging

--- bot.py
+++ bot.py
@@ -1,8 +1,10 @@
 from bot_functions.get_quote import quote_on_enable
 from bot_functions.reply import reply_on_enable
 from bot_functions.retweet import retweet_on_enable
+from bot_functions.nuke import nuke
 
 while True:
+    nuke()
     quote_on_enable()
     reply_on_enable()
     retweet_on_enable()
