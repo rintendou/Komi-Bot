d98d50f6c65fe3ebb37d74fa82c5867daff6dc38
Danny Chen <dannychen8102@gmail.com>
2022-04-12 17:14:46 -0400

    Updated initialzed class to new class name

--- bot.py
+++ bot.py
@@ -1,12 +1,14 @@
-from bot_functions.get_quote import quote_on_enable
-from bot_functions.reply import reply_on_enable
-from bot_functions.retweet import retweet_on_enable
-from bot_functions.nuke import nuke
+from bot_functions.Moderation import Moderation
+from bot_functions.Mention import Mention
+from bot_functions.Tweet import Tweet
 
+# Initializing all classes.
+mod = Moderation()
+mention = Mention()
+tweet = Tweet()
+
+# Nuke on run.
+mod.nuke()
 
 while True:
-    nuke()
-    get_gif()
-    quote_on_enable()
-    reply_on_enable()
-    retweet_on_enable()
+    
