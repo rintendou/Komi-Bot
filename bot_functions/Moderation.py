b49e7b7e314428747d0e01f5af4a784997ffa515
Danny Chen <dannychen8102@gmail.com>
2022-04-12 09:49:57 -0400

    Improved nuke() method by providing an exit condition of 'If no tweets posted, abort nuke process'

--- bot_functions/Moderation.py
+++ bot_functions/Moderation.py
@@ -2,11 +2,13 @@ import credentials.setup
 import tweepy
 
 api = credentials.setup.setup()
-
+bot_id = int(api.verify_credentials().id_str)
 class Moderation:
     def nuke(self):
-        tweets = tweepy.Cursor(api.user_timeline).items()
-        if tweets != None:
+        tweets = api.user_timeline()
+        if not tweets: 
+           print("Empty timeline, aborting nuke process" + '\n' + "========")
+        else: 
             print("You are about to delete all of Komi's tweets. Are you sure you want to continue? (Y/N)")
             delete = input("> ")
             if delete.lower() == 'y' or delete.upper() == 'Y':
@@ -14,5 +16,3 @@ class Moderation:
                         api.destroy_status(tweet.id)
                         print("Deleted: " , tweet.id, '\n')
                 print("Successfully nuked Komi's timeline." + '\n' + "========")
-        else: 
-            print("Empty timeline, skipping nuke process" + '\n' + "========")
